6207ec8b9e0a5f12d84c0d81e40aa8e5e68e356f
avendesta <53615807+avendesta@users.noreply.github.com>
2022-02-18 16:28:16 -0600

    Added a layout fragment

--- blog/src/main/java/main/blog/controllers/Home.java
+++ blog/src/main/java/main/blog/controllers/Home.java
@@ -1,15 +1,12 @@
 package main.blog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Home {
-    @GetMapping("{username}")
-    public String index(@PathVariable String username, Model model){
-        model.addAttribute("name",username);
+    @GetMapping("/")
+    public String index(){
         return "index";
     }
 }
