5804a142e2b36d81cfc9059ea5a3d1dd83e2a678
avendesta <53615807+avendesta@users.noreply.github.com>
2022-02-18 16:41:14 -0600

    Fixed page title

--- blog/src/main/java/main/blog/controllers/Home.java
+++ blog/src/main/java/main/blog/controllers/Home.java
@@ -1,12 +1,14 @@
 package main.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Home {
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("pageTitle", "Dashboard Page");
         return "index";
     }
 }
