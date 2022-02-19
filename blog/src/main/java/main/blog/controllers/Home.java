57b9b0231ababbd90b60ecf94b830cd3698d6b0a
avendesta <53615807+avendesta@users.noreply.github.com>
2022-02-19 15:06:28 -0600

    simple css style added

--- blog/src/main/java/main/blog/controllers/Home.java
+++ blog/src/main/java/main/blog/controllers/Home.java
@@ -3,12 +3,13 @@ package main.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Home {
     @GetMapping("/")
     public String index(Model model){
-        model.addAttribute("pageTitle", "Dashboard Page");
+        model.addAttribute("pageTitle", "Blog");
         return "index";
     }
 }
